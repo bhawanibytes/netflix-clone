36c3f7bb458ec3d6f5a41f986bb2c22e29f3fc67
Bhawani Singh <82741086+bhawanibytes@users.noreply.github.com>
2023-12-01 00:04:23 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -8,8 +8,9 @@ function submitForm() {
         (enteredUsername === "bhawanibytes@gmail.com" || enteredUsername === "akashrajbytes@gmail.com") &&
         enteredPassword === "wewillrock"
     ) {
-        // Form will submit to "index.html" due to the form's action attribute
-        return true;
+        // Programmatic redirect using JavaScript
+        window.location.href = 'home.html';
+        return false; // Prevents the form from submitting via traditional means
     } else {
         // Display an error message
         alert("Invalid username or password. Please try again.");
