dda3914b64186dc14f504978c05e426a34816069
Bhawani Singh <82741086+bhawanibytes@users.noreply.github.com>
2023-12-01 03:40:14 +0530

    added login system

---
+++ login-system/validation.js
@@ -0,0 +1,19 @@
+function submitForm() {
+    // Get values from the form
+    var enteredUsername = document.getElementById("username").value;
+    var enteredPassword = document.getElementById("password").value;
+
+    // Validate against predefined values
+    if (
+        (enteredUsername === "bhawanibytes@gmail.com" || enteredUsername === "akashrajbytes@gmail.com") &&
+        enteredPassword === "wewillrock"
+    ) {
+        // Programmatic redirect using JavaScript
+        window.location.href = 'home/home.html';
+        return false; // Prevents the form from submitting via traditional means
+    } else {
+        // Display an error message
+        alert("Invalid username or password. Please try again.");
+        return false; // Prevents the form from submitting via traditional means
+    }
+}
